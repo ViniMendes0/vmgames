072a95d21f9166f73f5432b34147a0555c24a6f5
Vinicius Mendes <mendesvv96@gmail.com>
2025-07-09 17:57:25 -0300

    corriginfo

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import LandingPage from "./components/LandingPage";
-import VitrineJogos from "./components/Vitrinejogos";
+import VitrineJogos from './components/VitrineJogos.jsx';
+
 
 export default function VitrinePage() {
   return (
